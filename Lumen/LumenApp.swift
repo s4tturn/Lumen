aaaa05afa8dbd92aea6f0c8616263881a1ed5646
s4tturn <s4tturn@icloud.com>
2026-08-15 12:53:05 +0530

    Initial Commit

---
+++ Lumen/LumenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumenApp.swift
+//  Lumen
+//
+//  Created by Satyajit Rajadhyaksha on 15/08/26.
+//
+
+import SwiftUI
+
+@main
+struct LumenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
